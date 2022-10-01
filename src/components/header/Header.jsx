638ba08ca47bb8894b7691d5f68bef5f5cad4aff
lucasbear <lucasbear.yo@outlook.com>
2022-10-01 17:53:23 -0500

    Header updated

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,53 @@
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+import ME from "../../assets/img/me.png";
+import ReactTyped from "react-typed";
 import "./header.css";
-
+import "react-typed/dist/animatedCursor.css";
+import React from "react";
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header id="me">
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>
+          <ReactTyped
+            strings={["Carlos Lucas"]}
+            smartBackspace
+            typeSpeed={150}
+            backSpeed={60}
+            startDelay={100}
+            backDelay={55}
+            loop
+          />
+        </h1>
+        <h5 className="text-light">
+          Software{" "}
+          <span>
+            <ReactTyped
+              strings={["Enginee", "Develope", "Code"]}
+              smartBackspace
+              typeSpeed={70}
+              backSpeed={65}
+              cursorChar="r"
+              loop
+            />
+          </span>
+        </h5>
 
-export default Header
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="Person profile" title="Yep, that's me!" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
